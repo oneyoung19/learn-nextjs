abc0a8f14d933af43e73473059b80c9f9265a77c
Andy Yang <2878847069@qq.com>
2025-06-14 19:15:14 +0800

    refactor(app/data-fetching): 在app router下实现类似于nuxt的渲染机制

--- app/data-fetching/posts.tsx
+++ app/data-fetching/posts.tsx
@@ -1,6 +1,6 @@
 'use client'
+import * as React from 'react'
 
-import { useState, useEffect } from 'react'
 import { useQuery } from '@tanstack/react-query'
 
 const postsUrl = 'https://jsonplaceholder.typicode.com/posts'
@@ -10,22 +10,41 @@ async function fetchPosts() {
 	return res.json()
 }
 
-export default function Posts(props) {
-	const [posts, setPosts] = useState(props.posts || [])
+const TestRender = React.memo(function TestRender() {
+	// 使用 useEffect 追踪组件的挂载和更新
+	React.useEffect(() => {
+		console.log('TestRender mounted')
+		return () => {
+			console.log('TestRender unmounted')
+		}
+	}, [])
 
-	const { data, refetch } = useQuery({
-		queryKey: ['posts'],
-		queryFn: fetchPosts,
-		initialData: props.posts, // 使用服务端预渲染的数据作为初始数据
-		enabled: false, // 默认不自动获取数据
+	// 使用 useLayoutEffect 追踪实际的渲染
+	React.useLayoutEffect(() => {
+		console.log('TestRender layout effect')
 	})
 
-	// 当数据更新时，更新本地状态
-	useEffect(() => {
-		if (data) {
-			setPosts(data)
-		}
-	}, [data])
+	console.log('TestRender render')
+	return (
+		<p>TestRender</p>
+	)
+})
+// function TestRender() {
+// 	console.log('TestRender')
+// 	return (
+// 		<p>TestRender</p>
+// 	)
+// }
+
+export default function Posts(props) {
+	// const [posts, setPosts] = useState(props.posts || [])
+	// 直接使用 React Query 的数据，不需要额外的 state
+	const { data: posts = props.posts, refetch } = useQuery({
+		queryKey: ['posts'],
+		queryFn: fetchPosts,
+		initialData: props.posts,
+		enabled: false,
+	})
 
 	return (
 		<div>
@@ -35,6 +54,7 @@ export default function Posts(props) {
 			>
 				刷新数据
 			</button>
+			<TestRender />
 			<ul>
 				{
 					posts.map(post => (
